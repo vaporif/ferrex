948dedefbc65bc548111decd4e83cb71807e6179
Dmytro Onypko <vaporif@proton.me>
2026-04-07 16:30:22 +0100

    upd

--- flake.nix
+++ flake.nix
@@ -71,7 +71,7 @@
           # Full integration tests run in CI integration job.
           cargoTestExtraArgs = "--workspace --exclude ferrex-embed --exclude ferrex-server --lib";
           postInstall = ''
-            wrapProgram $out/bin/server \
+            wrapProgram $out/bin/ferrex-server \
               --set ORT_DYLIB_PATH "${pkgs.onnxruntime}/lib/libonnxruntime${pkgs.stdenv.hostPlatform.extensions.sharedLibrary}" \
               --prefix PATH : "${pkgs.qdrant}/bin"
           '';
